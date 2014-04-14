de4572d63ee328d4d16b23bebeaa286bae63db41
andy-walker <andyw+github2@circle-interactive.co.uk>
2014-04-14 16:10:40 +0100

    whitespace

--- styler.js
+++ styler.js
@@ -12,46 +12,46 @@ cj('.crm-container .crm-button input[type=button], .crm-container .crm-button in
 // for each checkbox, create wrapper div and add label after element
 cj('.crm-container input[type=checkbox]').each(function() {
 
-	// if checkbox has no id, create one based on the name
-	if (!cj(this).attr('id'))
-		cj(this).attr('id', cj(this).attr('name').replace(/[\[\]]/g, '_'));
+    // if checkbox has no id, create one based on the name
+    if (!cj(this).attr('id'))
+        cj(this).attr('id', cj(this).attr('name').replace(/[\[\]]/g, '_'));
 
-	// add wrapper div and label after checkbox
-	cj(this).wrap('<div class="styled-checkbox"></div>')
-	        .after('<label for="' + cj(this).attr('id') + '"></label>');
+    // add wrapper div and label after checkbox
+    cj(this).wrap('<div class="styled-checkbox"></div>')
+            .after('<label for="' + cj(this).attr('id') + '"></label>');
 });
 
 // for each radio button, create wrapper div and add label after element
 cj('.crm-container input[type=radio]').each(function() {
 
-	// if checkbox has no id, create one based on the name
-	if (!cj(this).attr('id'))
-		cj(this).attr('id', cj(this).attr('name').replace(/[\[\]]/g, '_'));
+    // if checkbox has no id, create one based on the name
+    if (!cj(this).attr('id'))
+        cj(this).attr('id', cj(this).attr('name').replace(/[\[\]]/g, '_'));
 
-	// add wrapper div and label after checkbox
-	cj(this).wrap('<div class="styled-radio"></div>')
-	        .after('<label for="' + cj(this).attr('id') + '"></label>');
+    // add wrapper div and label after checkbox
+    cj(this).wrap('<div class="styled-radio"></div>')
+            .after('<label for="' + cj(this).attr('id') + '"></label>');
 });
 
 // add wrapper div to select boxes
 cj('.crm-container select').each(function() {
-	var width = cj(this).width();
-	cj(this).wrap('<div class="select-box-wrapper" style="width:' + width + 'px"></div>');
-	
-	// make select box 20px wider, cutting off arrow due to overflow:hidden on wrapper
-	cj(this).width(width + 20)
-	cj(this).focus(function() {
-				cj(this).parent().addClass('select-box-wrapper-focused');
-			})
-			.blur(function() {
-				cj(this).parent().removeClass('select-box-wrapper-focused');
-			});
+    var width = cj(this).width();
+    cj(this).wrap('<div class="select-box-wrapper" style="width:' + width + 'px"></div>');
+    
+    // make select box 20px wider, cutting off arrow due to overflow:hidden on wrapper
+    cj(this).width(width + 20)
+    cj(this).focus(function() {
+                cj(this).parent().addClass('select-box-wrapper-focused');
+            })
+            .blur(function() {
+                cj(this).parent().removeClass('select-box-wrapper-focused');
+            });
 });
 
 
 cj('.crm-container select option:first-child').each(function() {
-	var label = cj(this).html();
-	if (label.match(/^\-.+\-$/)) {
-		cj(this).html('<span class="option-first">' + label.replace(/^\-(.+)\-$/, '$1...') + '</span>');
-	}
+    var label = cj(this).html();
+    if (label.match(/^\-.+\-$/)) {
+        cj(this).html('<span class="option-first">' + label.replace(/^\-(.+)\-$/, '$1...') + '</span>');
+    }
 });
--- styler.php
+++ styler.php
@@ -5,18 +5,18 @@
  */
 function styler_civicrm_config() {
 
-	// add styles
-	CRM_Core_Resources::singleton()->addStyleFile(
-		'com.uk.andyw.styler', 
-		'default.css',
-		CRM_Core_Resources::DEFAULT_WEIGHT,
+    // add styles
+    CRM_Core_Resources::singleton()->addStyleFile(
+        'com.uk.andyw.styler', 
+        'default.css',
+        CRM_Core_Resources::DEFAULT_WEIGHT,
         'html-header'
-	);
- 	
- 	// add javascript
-	CRM_Core_Resources::singleton()->addScriptFile(
-		'com.uk.andyw.styler', 
-		'styler.js'	
-	);
+    );
+    
+    // add javascript
+    CRM_Core_Resources::singleton()->addScriptFile(
+        'com.uk.andyw.styler', 
+        'styler.js' 
+    );
 
 }
